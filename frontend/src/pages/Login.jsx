c7128ae45f9178ed0b5a4d4c85e77d87b78df189
px1412 <avinashmehta2016fzk@gmail.com>
2024-03-08 16:18:10 +0530

    MAKING LOGIN PAGE

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import { Button, Container, Paper, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [isLogin, setIsLogin] = useState(true);
+  return (
+    <Container>
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: flex,
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {isLogin ? (
+          <>
+            <Typography variant="h5">Login</Typography>
+            <form>
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                variant="contained"
+                color="primary"
+              >
+                Login
+              </Button>
+              <Typography>Or</Typography>
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                }}
+                variant="contained"
+                color="secondary"
+                onClick={() => setIsLogin(false)}
+              >
+                Register
+              </Button>
+            </form>
+          </>
+        ) : (
+          <span>register</span>
+        )}
+      </Paper>
+    </Container>
+  );
 };
 
 export default Login;
